f682f524c81d25b88689a54bcaa54eba5a04bd0a
Krakenfall <krakenfall@gmail.com>
2017-11-02 14:32:32 -0700

    Fixed accidental committed code

--- config.transform.js
+++ config.transform.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 
 // First argument is build configuration
-const build_cnf = 'Releases';// process.argv[2].toLowerCase();
+const build_cnf = process.argv[2].toLowerCase();
 
-try {
-    const transform_content = fs.readFileSync(`config.${build_cnf}.json`);
-    fs.writeFileSync('config.json', transform_content)
-} catch (e) {
-    console.log(e);
-}
+const transform_content = fs.readFileSync(`config.${build_cnf}.json`);
+fs.writeFileSync('config.json', transform_content);
